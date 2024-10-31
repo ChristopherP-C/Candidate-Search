4b8a96972150e19fe65c20a4b3a17f534a24270a
ChristopherP-C <christopherp.c.2day@gmail.com>
2024-10-31 05:31:51 -0500

    added candidate interface

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,11 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export default interface Candidate {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    avatar: string;
+    location?: string;
+    company?: string;
+    bio?: string;
+}
